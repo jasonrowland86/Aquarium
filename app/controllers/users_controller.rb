48309ef0e428ea0e1077f6dca46cfaeb1d2c3d87
Jason Rowland <jasonrowland86>
2017-09-23 11:33:52 -0400

    Created users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def index
+    @user_count = User.count
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(create_params)
+
+    if user.save
+      sign_in(user)
+      flash[:notice] = 'Created Account!'
+      redirect_to users_path
+    else
+      errors = user.errors.full_messages
+      flash[:error] = errors.join(', ')
+      @user = User.new(username: create_params[:username])
+      render :new
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:user).permit(:username, :password)
+  end
+end
